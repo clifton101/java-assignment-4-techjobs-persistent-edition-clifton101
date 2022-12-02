90b27113e646ab77595736b83690992d4384e91d
micha <michaclifton@yahoo.com>
2022-12-01 19:23:02 -0800

    Finishing up Part 4

--- queries.sql
+++ queries.sql
@@ -11,3 +11,7 @@ WHERE location = "St. Louis City";
 DROP TABLE job;
 
 -- Part 4: Test it with SQL
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE jobs_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
